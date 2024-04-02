add373a81bfe9b424ae119492da49f2c1d370899
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-04-01 22:36:53 -0700

    added temporary solution to the weapons changing size based on number

--- src/components/WeaponList.jsx
+++ src/components/WeaponList.jsx
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Weapon from './Weapon.jsx';
 
 const WeaponList = ({ weapons, addWeapon }) => {
+  const weaponBoat = [];
+  let weaponDinghy = [];
+
+  for (var i = 0; i < weapons.length; i++) {
+    weaponDinghy.push(<Weapon weapon={weapons[i]} changeGrid={addWeapon}/>);
+    if ((i+1) % 5 === 0 || i+1 === weapons.length) {
+      weaponBoat.push(<div className="weaponBoat">{weaponDinghy}</div>);
+      weaponDinghy = [];
+    }
+  }
   return (
     <ul className="weaponList">
-      {weapons.length > 0 && weapons.map((weapon) => {
-        return <Weapon weapon={weapon} changeGrid={addWeapon}/>
-      })}
+      {weaponBoat}
     </ul>
   )
 };
